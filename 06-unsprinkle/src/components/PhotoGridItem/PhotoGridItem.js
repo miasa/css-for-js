20807e80f60cd2c1c0ead2ada4a26313d3e43e6e
Mia <mia@crasman.fi>
2022-07-08 14:34:51 +0300

    Excercise 6: unsprinkle

---
+++ 06-unsprinkle/src/components/PhotoGridItem/PhotoGridItem.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+const PhotoGridItem = ({ id, src, alt, tags }) => {
+  return (
+    <article>
+      <Anchor href={`/photos/${id}`}>
+        <picture>
+          <source
+            type="image/avif"
+            srcSet={`
+              ${src.replace('.jpg', '.avif')} 1x,
+              ${src.replace('.jpg', '@2x.avif')} 2x,
+              ${src.replace('.jpg', '@3x.avif')} 3x
+            `}
+          ></source>
+          <source
+            type="image/jpg"
+            srcSet={`
+              ${src.replace('.jpg', '.jpg')} 1x,
+              ${src.replace('.jpg', '@2x.jpg')} 2x,
+              ${src.replace('.jpg', '@3x.jpg')} 3x
+            `}
+          ></source>
+          <Image src={src} alt={alt} />
+        </picture>
+      </Anchor>
+      <Tags>
+        {tags.map((tag) => (
+          <Tag key={tag}>{tag}</Tag>
+        ))}
+      </Tags>
+    </article>
+  );
+};
+
+const Anchor = styled.a`
+  text-decoration: none;
+  color: inherit;
+  outline-offset: 4px;
+`;
+
+const Image = styled.img`
+  display: block;
+  width: 100%;
+  height: 300px;
+  border-radius: 2px;
+  margin-bottom: 8px;
+  object-fit: cover;
+`;
+
+const Tags = styled.ul`
+  overflow: hidden;
+  padding: 4px 0;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
+const Tag = styled.li`
+  padding: 4px 8px;
+  background: var(--color-gray-300);
+  font-size: 0.875rem;
+  font-weight: 475;
+  color: var(--color-gray-800);
+  display: inline;
+
+  &:not(:last-child) {
+    margin-right: 8px;
+  }
+`;
+
+export default PhotoGridItem;
